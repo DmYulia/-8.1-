ec04dde6fe5fcbf1bf9b97cc1a9d3b72991111e0
Юлия <@LAPTOP-B0K7S1UE>
2022-01-24 20:08:03 +0700

    Добавьте файлы проекта.

---
+++ файловая система 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace файловая_система_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Begin:
+            Console.Write("Введите полное имя папки: C:\\");
+            string userdir = Console.ReadLine();
+            string dirname = "C:\\" + userdir;
+            if (Directory.Exists(dirname))
+            {
+                string[] files = Directory.GetFiles(dirname);
+                foreach (string s in files)
+                {
+                    Console.WriteLine($"--- {s}");
+                }
+
+                string[] dirs = Directory.GetDirectories(dirname);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine($"-↓- {s}");
+                    string[] files2 = Directory.GetFileSystemEntries(s);
+                    foreach (string s2 in files2)
+                    {
+                        Console.WriteLine($"    --- {s2}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка! Папка не существует!");
+                goto Begin;
+            }
+            Console.ReadLine();
+        }
+    }
+}
